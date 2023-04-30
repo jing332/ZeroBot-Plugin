bd473b66a85c0a8f81d9fd658d57c3e1c37fc4ac
Jing <42014615+jing332@users.noreply.github.com>
2023-04-30 16:52:44 +0800

    1

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	webctrl "github.com/FloatTech/zbputils/control/web"
 	"math/rand"
 	"os"
 	"runtime"
@@ -218,11 +219,11 @@ func init() {
 	d := flag.Bool("d", false, "Enable debug level log and higher.")
 	w := flag.Bool("w", false, "Enable warning level log and higher.")
 	h := flag.Bool("h", false, "Display this help.")
-	// g := flag.String("g", "127.0.0.1:3000", "Set webui url.")
+	g := flag.String("g", "127.0.0.1:3000", "Set webui url.")
 	// 直接写死 AccessToken 时，请更改下面第二个参数
 	token := flag.String("t", "", "Set AccessToken of WSClient.")
 	// 直接写死 URL 时，请更改下面第二个参数
-	url := flag.String("u", "ws://127.0.0.1:6700", "Set Url of WSClient.")
+	url := flag.String("u", "ws://192.168.0.113:6700", "Set Url of WSClient.")
 	// 默认昵称
 	adana := flag.String("n", "椛椛", "Set default nickname.")
 	prefix := flag.String("p", "/", "Set command prefix.")
@@ -256,10 +257,10 @@ func init() {
 
 	// 通过代码写死的方式添加主人账号
 	// sus = append(sus, 12345678)
-	// sus = append(sus, 87654321)
+	sus = append(sus, 3033784236)
 
 	// 启用 webui
-	// go webctrl.RunGui(*g)
+	go webctrl.RunGui(*g)
 
 	if *runcfg != "" {
 		f, err := os.Open(*runcfg)
